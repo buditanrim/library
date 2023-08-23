ff4eb6f2a5783cb2cdcb8120dd46eaed7da86cca
Buditanrim <1830184+buditanrim@users.noreply.github.com>
2023-08-23 14:31:14 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = []; // empty array
+
+// constructor
+function Book(title, author, pages) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+}
+
+function addBookToLibrary() {
+    
+}
